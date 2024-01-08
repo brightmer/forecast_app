52285f5b364a018bc3b531c850109951d4d8e632
Brett Rightmer <brett.rightmer@gmail.com>
2024-01-08 10:31:02 -0800

    Added annotations

--- test/models/forecast_test.rb
+++ test/models/forecast_test.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: forecasts
+#
+#  id                  :bigint           not null, primary key
+#  current_temperature :float(24)
+#  forecast_at         :datetime
+#  high_temperature    :float(24)
+#  low_temperature     :float(24)
+#  units               :string(255)
+#  weather_description :string(255)
+#  weather_icon        :string(255)
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  weather_location_id :bigint           not null
+#
+# Indexes
+#
+#  index_forecasts_on_weather_location_id  (weather_location_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (weather_location_id => weather_locations.id)
+#
 require "test_helper"
 
 class ForecastTest < ActiveSupport::TestCase
--- test/models/weather_location_test.rb
+++ test/models/weather_location_test.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: weather_locations
+#
+#  id                  :bigint           not null, primary key
+#  address             :text(65535)
+#  current_temperature :float(24)
+#  date_checked        :datetime
+#  high_temperature    :float(24)
+#  latitude            :float(24)
+#  longitude           :float(24)
+#  low_temperature     :float(24)
+#  postal_code         :string(255)
+#  units               :string(255)
+#  weather_description :string(255)
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
 require "test_helper"
 
 class WeatherLocationTest < ActiveSupport::TestCase
